4e6f420c8d620a4e928343f5efa1a12c61522557
Guillaume Rose <guillaume.rose@mappy.com>
2015-02-12 14:02:59 +0100

    Add logs

--- src/main/java/fr/guillaumerose/App.java
+++ src/main/java/fr/guillaumerose/App.java
@@ -3,12 +3,11 @@ package fr.guillaumerose;
 import static fr.guillaumerose.Map.Direction.*;
 
 public class App {
-
     public static void main(String[] args) {
         Map map = new Map(100, 100);
         Turtle turtle = new Turtle(map, 0, 0, N);
         turtle.forward();
-        turtle.turnLeft();
+        turtle.turnRight();
         turtle.forward();
         turtle.forward();
         turtle.forward();
--- src/main/java/fr/guillaumerose/Turtle.java
+++ src/main/java/fr/guillaumerose/Turtle.java
@@ -4,15 +4,20 @@ import fr.guillaumerose.Map.Direction;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Data
 @AllArgsConstructor
 public class Turtle {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Turtle.class);
     private final Map map;
     private int x;
     private int y;
     private Direction direction;
 
     public void forward() {
+        LOGGER.info("Going forward. " + summary());
         switch (direction) {
         case N:
             y = map.getMaxY() >= y + 1 ? y + 1 : y;
@@ -30,10 +35,16 @@ public class Turtle {
     }
 
     public void turnLeft() {
+        LOGGER.info("Turn left. " + summary());
         direction = direction.getLeft();
     }
 
     public void turnRight() {
+        LOGGER.info("Turn right. " + summary());
         direction = direction.getRight();
+    }
+
+    private String summary() {
+        return "(" + x + ", " + y + ", " + direction + ")";
     }
 }
