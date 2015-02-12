18c343e4b48a624d7ac8bd3f96d108f340791d2a
Guillaume Rose <guillaume.rose@mappy.com>
2015-02-12 13:36:23 +0100

    Add domain-model

--- src/main/java/fr/guillaumerose/App.java
+++ src/main/java/fr/guillaumerose/App.java
@@ -1,7 +1,48 @@
 package fr.guillaumerose;
 
+import lombok.Data;
+
+import static fr.guillaumerose.App.Direction.*;
+
 public class App {
+
+    public static enum Direction {
+        N, S, E, W;
+    }
+
+    @Data
+    public static class Map {
+        private final int maxX;
+        private final int maxY;
+    }
+
+    @Data
+    public static class Turtle {
+        private final Map map;
+        private int x = 0;
+        private int y = 0;
+        private Direction direction = N;
+
+        public void forward() {
+
+        }
+
+        public void turnLeft() {
+
+        }
+
+        public void turnRight() {
+
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Map map = new Map(100, 100);
+        Turtle turtle = new Turtle(map);
+        turtle.forward();
+        turtle.turnLeft();
+        turtle.forward();
+        turtle.forward();
+        turtle.forward();
     }
 }
